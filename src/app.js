c8d884eccac091a1dc426b3105fb770ec9bf5991
cbstodd <cbstodd@gmail.com>
2023-04-06 23:00:39 -0400

    Commit before adding Express

--- src/app.js
+++
@@ -1,6 +0,0 @@
-var earthtone = ['Andy', 'Colin', 'Marshall', 'Rick', 'Tommy'];
-console.table(earthtone);
-var earthtoneArray = earthtone.map(function (_user) {
-    return "".concat(_user, " plays in Earthtone");
-});
-console.table(earthtoneArray);
